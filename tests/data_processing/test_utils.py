5ef31b2ee537ae11bd95fdfd9e279a3f8181c452
William Graf <7930703+willgraf@users.noreply.github.com>
2018-12-03 19:52:15 -0800

    updated comment

--- tests/data_processing/test_utils.py
+++ tests/data_processing/test_utils.py
@@ -23,7 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Tests for pre-processing functions"""
+"""Tests for utility functions"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
