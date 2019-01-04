2e9fc2efc5ba4e8a3dd32280600f096dec605d43
William Graf <7930703+willgraf@users.noreply.github.com>
2019-01-03 17:06:04 -0800

    add copyright, fix imports, put server in __main__ block, env var LISTEN_PORT

--- server.py
+++ server.py
@@ -1,17 +1,45 @@
-import grpc
-from concurrent import futures
+# Copyright 2016-2019 The Van Valen Lab at the California Institute of
+# Technology (Caltech), with support from the Paul Allen Family Foundation,
+# Google, & National Institutes of Health (NIH) under Grant U24CA224309-01.
+# All rights reserved.
+#
+# Licensed under a modified Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.github.com/vanvalenlab/kiosk-data-processing/LICENSE
+#
+# The Work provided may be used for non-commercial academic purposes only.
+# For any other use of the Work, including commercial use, please contact:
+# vanvalenlab@gmail.com
+#
+# Neither the name of Caltech nor the names of its contributors may be used
+# to endorse or promote products derived from this software without specific
+# prior written permission.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""gRPC server to expose data processing functions"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
 import time
 
-# import the generated classes
+import grpc
+from concurrent import futures
+
 from data_processing.pbs import process_pb2
 from data_processing.pbs import processing_service_pb2
 from data_processing.pbs import processing_service_pb2_grpc
 
-# import the processing functions
 from data_processing.utils import get_function
 
-
-PORT = 8080
 
 # create a class to define the server functions, derived from
 # calculator_pb2_grpc.CalculatorServicer
@@ -27,23 +55,25 @@ class ProcessingServicer(processing_service_pb2_grpc.ProcessingServiceServicer):
         return response
 
 
-# create a gRPC server
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+if __name__ == '__main__':
+    LISTEN_PORT = os.getenv('LISTEN_PORT', 8080)
 
-# use the generated function `add_ProcessingServicer_to_server`
-# to add the defined class to the server
-processing_service_pb2_grpc.add_ProcessingServiceServicer_to_server(
-        ProcessingServicer(), server)
+    # create a gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
-# listen on port 50051
-print('Starting server. Listening on port', PORT)
-server.add_insecure_port('[::]:{}'.format(PORT))
-server.start()
+    # use the generated function `add_ProcessingServicer_to_server`
+    # to add the defined class to the server
+    processing_service_pb2_grpc.add_ProcessingServiceServicer_to_server(
+            ProcessingServicer(), server)
 
-# since server.start() will not block,
-# a sleep-loop is added to keep alive
-try:
-    while True:
-        time.sleep(86400)  # 24 hours
-except KeyboardInterrupt:
-    server.stop(0)
+    print('Starting server. Listening on port', LISTEN_PORT)
+    server.add_insecure_port('[::]:{}'.format(LISTEN_PORT))
+    server.start()
+
+    # since server.start() will not block,
+    # a sleep-loop is added to keep alive
+    try:
+        while True:
+            time.sleep(86400)  # 24 hours
+    except KeyboardInterrupt:
+        server.stop(0)
